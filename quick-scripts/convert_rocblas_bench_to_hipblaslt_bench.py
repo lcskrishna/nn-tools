64f3a23925a0fc75a598ebfbf21a8f1d92c23e83
lcskrishna <lollachaitanya@gmail.com>
2024-09-26 08:03:39 +0000

    convert rocblas bench to hipblaslt bench

---
+++ quick-scripts/convert_rocblas_bench_to_hipblaslt_bench.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import argparse
+
+def generate_hipblaslt_commands(rocblas_script):
+    fs = open(rocblas_script, 'r')
+    lines = fs.readlines()
+    fs.close()
+
+    commands = []
+    for j in range(len(lines)):
+        line = lines[j].rstrip()
+        if "rocblas-bench" in line:
+            line = line.replace("rocblas-bench", "hipblaslt-bench")
+        if "transposeA" in line:
+            line = line.replace("transposeA", "transA")
+        if "transposeB" in line:
+            line = line.replace("transposeB", "transB")
+        if "gemm" in line:
+            line = line.replace("gemm", "matmul")
+        if "gemm_ex" in line:
+            line = line.replace("gemm_ex", "matmul")
+        commands.append(line)
+    return commands
+
+def dump_hipblaslt_commands(commands):
+    fs = open("hipblaslt-script.sh", "w")
+    for j in range(len(commands)):
+        fs.write(commands[j])
+        fs.write("\n")
+    fs.close()
+    print ("OK: dumped hipblaslt-script.sh")
+
+def main():
+    commands = os.path.abspath(args.rocblas_script)
+    dump_hipblaslt_commands(commands)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rocblas-script", type=str, required=True)
+    args = parser.parse_args()
+
+    main() 
